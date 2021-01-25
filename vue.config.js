6ad625d7f4d2c9ae9d87138220b43e33ed1fb126
张宇 <776210576@qq.com>
2021-01-25 21:03:23 +0800

    生产环境bug fix demo

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
  * @Author: ZY
  * @Date: 2020-12-07 11:41:22
  * @LastEditors: ZY
- * @LastEditTime: 2021-01-23 16:20:30
+ * @LastEditTime: 2021-01-25 21:02:30
  */
 const { resolve } = require('path')
 const path = require('path')
@@ -72,67 +72,4 @@ module.exports = {
       ]
     }
   },
-  chainWebpack(config) {
-    // provide the app's title in html-webpack-plugin's options list so that
-    // it can be accessed in index.html to inject the correct title.
-    // https://cli.vuejs.org/guide/webpack.html#modifying-options-of-a-plugin
-    config.plugin('html').tap(args => {
-      args[0].title = title
-      return args
-    })
-
-    // it can improve the speed of the first screen, it is recommended to turn on preload
-    config.plugin('preload').tap(() => [
-      {
-        rel: 'preload',
-        // to ignore runtime.js
-        // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
-        fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
-        include: 'initial'
-      }
-    ])
-
-    // when there are many pages, it will cause too many meaningless requests
-    config.plugins.delete('prefetch')
-
-    // https://webpack.js.org/configuration/devtool/#development
-    // Change development env source map if you want.
-    // The default in vue-cli is 'eval-cheap-module-source-map'.
-    // config
-    //   .when(process.env.NODE_ENV === 'development',
-    //     config => config.devtool('eval-cheap-source-map')
-    //   )
-
-    config
-      .when(process.env.NODE_ENV !== 'development',
-        config => {
-          config
-            .optimization.splitChunks({
-              chunks: 'all',
-              cacheGroups: {
-                libs: {
-                  name: 'chunk-libs',
-                  test: /[\\/]node_modules[\\/]/,
-                  priority: 10,
-                  chunks: 'initial' // only package third parties that are initially dependent
-                },
-                elementUI: {
-                  name: 'chunk-elementUI', // split elementUI into a single package
-                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-                },
-                commons: {
-                  name: 'chunk-commons',
-                  test: path.resolve(__dirname, 'src/components'),
-                  minChunks: 3, //  minimum common number
-                  priority: 5,
-                  reuseExistingChunk: true
-                }
-              }
-            })
-          // https://webpack.js.org/configuration/optimization/#optimizationruntimechunk
-          config.optimization.runtimeChunk('single')
-        }
-      )
-  }
 }
