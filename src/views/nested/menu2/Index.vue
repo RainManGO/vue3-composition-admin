597422112c07f3eada8ae45d93d5e557d17b7447
张宇 <776210576@qq.com>
2021-01-25 12:44:45 +0800

    add menu2

---
+++ src/views/nested/menu2/Index.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Description:
+ * @Autor: WJM
+ * @Date: 2021-01-18 14:53:33
+ * @LastEditors: ZY
+ * @LastEditTime: 2021-01-25 12:44:14
+-->
+<template>
+  <div style="padding:30px;">
+    <el-alert
+      :closable="false"
+      title="menu 2"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+})
+</script>
