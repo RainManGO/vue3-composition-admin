ef49c335cc5778c118ce907452c028f348c51686
M69W <M69W@M69W>
2021-03-25 23:59:10 +0800

    fix issue 44

--- src/plugins/element.ts
+++ src/plugins/element.ts
@@ -38,9 +38,10 @@
  */
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
+import i18n from '@/locales'
 import { useStore } from '@/store'
 export default function loadComponent(app: any) {
-  app.use(ElementPlus, { size: useStore().state.app.size })
+  app.use(ElementPlus, { size: useStore().state.app.size, i18n: i18n.global.t })
   // app.use(ElButton)
   // app.use(ElSelect)
   // app.use(ElBreadcrumb)
