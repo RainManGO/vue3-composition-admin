17fad0c88f15be7f4d6e6dc4b327cdc425b9e79f
ZY <776210576@qq.com>
2023-04-17 09:40:58 +0800

    feat:已集成tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,26 @@
+<!--
+ * @Author: ZY
+ * @Date: 2023-04-05 21:48:55
+ * @LastEditors: ZY
+ * @LastEditTime: 2023-04-15 22:56:37
+ * @FilePath: /vue3-composition-admin/src/App.vue
+ * @Description: 文件描述
+-->
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="container  bg-red-300 ">
+    123
+  </div>
+  <span class="decoration-clone bg-gradient-to-b from-yellow-400 to-red-500 text-transparent ">
+    Hello<br>
+    World
+  </span>
+  <!-- <RouterView /> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: ZY
+ * @Date: 2023-04-13 14:14:11
+ * @LastEditors: ZY
+ * @LastEditTime: 2023-04-15 20:35:13
+ * @FilePath: /vue3-composition-admin/tailwind.config.js
+ * @Description: 文件描述
+ */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
